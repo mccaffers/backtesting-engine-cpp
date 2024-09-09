73a7cf4573b015afe90f52c32404dec312928c1d
Ryan <16667079+mccaffers@users.noreply.github.com>
2024-09-09 10:40:55 +0100

    remove build directory, and add gitignore back in

--- build/CMakeFiles/BacktestingEngine.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for BacktestingEngine.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/BacktestingEngineLib.dir/source/tests/main_test.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/BacktestingEngineLib.dir/source/tests/main_test.cpp.o: \
-  /Users/ryan/dev/cpp/backtesting-engine-cpp/source/tests/main_test.cpp
