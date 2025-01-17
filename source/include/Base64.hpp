de87fd12785afb779bd2d26b4af61c17934263d8
Ryan <16667079+mccaffers@users.noreply.github.com>
2025-01-17 09:13:11 +0000

    fixed headers

--- source/include/Base64.hpp
+++ source/include/Base64.hpp
@@ -7,6 +7,9 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>  // for std::remove_if, std::all_of
+#include <cstring>   // for strchr
+#include <string>    // for std::string
 
 class Base64 {
 public:
