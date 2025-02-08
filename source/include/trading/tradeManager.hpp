86ae51a522fe560f476a43e2a8b78e834c21dfcf
Ryan <16667079+mccaffers@users.noreply.github.com>
2025-02-08 10:56:00 +0000

    Moving the logic for trade manager out of the header file into the source

--- source/include/trading/tradeManager.hpp
+++ source/include/trading/tradeManager.hpp
@@ -14,53 +14,14 @@ private:
     static TradeManager* instance;
     std::unordered_map<std::string, Trade> activeTrades;
     
-    // Private constructor for singleton
     TradeManager() = default;
 
 public:
-    static TradeManager* getInstance() {
-        if (instance == nullptr) {
-            instance = new TradeManager();
-        }
-        return instance;
-    }
-
-    // Add reset method for testing
-    static void reset() {
-        delete instance;
-        instance = nullptr;
-        Trade::resetCounter();  // Reset the trade ID counter
-    }
-    
-    // Clear all trades
-    void clearAllTrades() {
-        activeTrades.clear();
-    }
-
-    // Open a new trade
-    std::string openTrade(double price, double size, bool isLong) {
-        Trade trade(price, size, isLong);
-        activeTrades[trade.id] = trade;
-        return trade.id;
-    }
-
-    // Review account - returns number of open trades
-    size_t reviewAccount() const {
-        return activeTrades.size();
-    }
-
-    // Close a trade
-    bool closeTrade(const std::string& tradeId) {
-        auto it = activeTrades.find(tradeId);
-        if (it != activeTrades.end()) {
-            activeTrades.erase(it);
-            return true;
-        }
-        return false;
-    }
-
-    // Get active trades
-    const std::unordered_map<std::string, Trade>& getActiveTrades() const {
-        return activeTrades;
-    }
+    static TradeManager* getInstance();
+    static void reset();
+    void clearAllTrades();
+    std::string openTrade(double price, double size, bool isLong);
+    size_t reviewAccount() const;
+    bool closeTrade(const std::string& tradeId);
+    const std::unordered_map<std::string, Trade>& getActiveTrades() const;
 };
--- source/trading/tradeManager.cpp
+++ source/trading/tradeManager.cpp
@@ -7,3 +7,43 @@
 #include "tradeManager.hpp"
 
 TradeManager* TradeManager::instance = nullptr;
+
+TradeManager* TradeManager::getInstance() {
+    if (instance == nullptr) {
+        instance = new TradeManager();
+    }
+    return instance;
+}
+
+void TradeManager::reset() {
+    delete instance;
+    instance = nullptr;
+    Trade::resetCounter();
+}
+
+void TradeManager::clearAllTrades() {
+    activeTrades.clear();
+}
+
+std::string TradeManager::openTrade(double price, double size, bool isLong) {
+    Trade trade(price, size, isLong);
+    activeTrades[trade.id] = trade;
+    return trade.id;
+}
+
+size_t TradeManager::reviewAccount() const {
+    return activeTrades.size();
+}
+
+bool TradeManager::closeTrade(const std::string& tradeId) {
+    auto it = activeTrades.find(tradeId);
+    if (it != activeTrades.end()) {
+        activeTrades.erase(it);
+        return true;
+    }
+    return false;
+}
+
+const std::unordered_map<std::string, Trade>& TradeManager::getActiveTrades() const {
+    return activeTrades;
+}
