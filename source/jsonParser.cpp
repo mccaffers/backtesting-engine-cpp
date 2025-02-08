94ade91baa8f0db99ab5efbabb3968722ba39f12
Ryan <16667079+mccaffers@users.noreply.github.com>
2025-02-08 19:22:05 +0000

    Split off  JSON parser with more unit tests

---
+++ source/jsonParser.cpp
@@ -0,0 +1,26 @@
+#include "jsonParser.hpp"
+#include "base64.hpp"
+#include <iostream>
+
+using json = nlohmann::json;
+
+int JsonParser::parseConfigurationFromBase64(const std::string& input) {
+    // Ingest parameters
+    std::string output = Base64::b64decode(input);
+    
+    // Debug, console print out
+    std::cout << output;
+    
+    json j;
+    try {
+        j = json::parse(output);
+    }
+    catch (json::parse_error& ex) {
+        std::cerr << "parse error at byte " << ex.byte << std::endl;
+    }
+    
+    auto config = j.get<trading_definitions::Configuration>();
+    std::cout << config.RUN_ID << std::endl;
+
+    return 0;
+}
