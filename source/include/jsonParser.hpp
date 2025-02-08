12de2b70137ecc80ee99e5bcdd57d220226d59f8
Ryan <16667079+mccaffers@users.noreply.github.com>
2025-02-08 19:22:05 +0000

    Split off  JSON parser with more unit tests

---
+++ source/include/jsonParser.hpp
@@ -0,0 +1,16 @@
+// Backtesting Engine in C++
+//
+// (c) 2025 Ryan McCaffery | https://mccaffers.com
+// This code is licensed under MIT license (see LICENSE.txt for details)
+// ---------------------------------------
+
+#pragma once
+
+#include <string>
+#include <nlohmann/json.hpp>
+#include "trading_definitions.hpp"
+
+class JsonParser {
+public:
+    static int parseConfigurationFromBase64(const std::string& input);
+};
--- source/main.cpp
+++ source/main.cpp
@@ -21,32 +21,9 @@
 #include "base64.hpp"
 #include "trading_definitions.hpp"  // For everything
 #include "tradeManager.hpp"
+#include "jsonParser.hpp"
 
 using json = nlohmann::json;
-
-int parseJson(const std::string& input) {
-  
-  // Ingest parameters
-  std::string output = Base64::b64decode(input);
-  
-  // Debug, console print out
-  std::cout << output;
-  
-  json j;
-  try
-  {
-      j = json::parse(output);
-  }
-  catch (json::parse_error& ex)
-  {
-      std::cerr << "parse error at byte " << ex.byte << std::endl;
-  }
-  
-  auto config = j.get<trading_definitions::Configuration>();
-  std::cout << config.RUN_ID << std::endl;
-
-  return 0;
-}
 
 int main(int argc, const char * argv[]) {
   
@@ -54,7 +31,7 @@ int main(int argc, const char * argv[]) {
   DatabaseConnection db(argv[1], 8812, "qdb", "admin", "quest");
 
   // Load strategy from Base64 argv[2]
-  parseJson(argv[2]);
+  JsonParser::parseConfigurationFromBase64(argv[2]);
 
   // Example query - replace with your actual query
   std::string query = "SELECT * FROM EURUSD LIMIT 5;";
