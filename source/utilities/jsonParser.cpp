48a9a99d5c5b0eeb435639ac4b8f77d5e2f1fbcd
Ryan <16667079+mccaffers@users.noreply.github.com>
2026-04-12 09:47:21 +0100

    Refactoring

--- source/utilities/jsonParser.cpp
+++ source/utilities/jsonParser.cpp
@@ -1,6 +1,6 @@
 // Backtesting Engine in C++
 //
-// (c) 2025 Ryan McCaffery | https://mccaffers.com
+// (c) 2026 Ryan McCaffery | https://mccaffers.com
 // This code is licensed under MIT license (see LICENSE.txt for details)
 // ---------------------------------------
 
