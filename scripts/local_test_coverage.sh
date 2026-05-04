6a137245c5f3a82b8bd235790afdbbc3a8087774
Ryan <16667079+mccaffers@users.noreply.github.com>
2026-05-04 17:09:08 +0100

    Updated test coverage script and added auto pr response

--- scripts/local_test_coverage.sh
+++ scripts/local_test_coverage.sh
@@ -13,9 +13,6 @@ xcodebuild \
 -resultBundlePath TestResult/ \
 -enableCodeCoverage YES \
 -derivedDataPath "/tmp" \
-HEADER_SEARCH_PATHS="./external/libpqxx/include/pqxx/internal ./external/libpqxx/include/ ./external/libpqxx/build/include/ ./external/" \
-LIBRARY_SEARCH_PATHS="./external/libpqxx/src/ ./external/libpqxx/build/src/" \
-OTHER_LDFLAGS="-L./external/libpqxx/build/src -lpqxx -lpq -L/opt/homebrew/Cellar/pkgconf/2.3.0_1/lib -L/opt/homebrew/Cellar/pkgconf/2.3.0_1/lib/pkgconfig -L/opt/homebrew/Cellar/postgresql@14/14.15/lib/postgresql@14 -L/opt/homebrew/Cellar/postgresql@14/14.15/lib/postgresql@14/pgxs -L/opt/homebrew/Cellar/postgresql@14/14.15/lib/postgresql@14/pkgconfig" \
 clean build test
 
 bash ./.github/workflows/xccov-to-sonarqube-generic.sh *.xcresult/ > sonarqube-generic-coverage.xml
