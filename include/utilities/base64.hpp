947c3a8d633a1b8f627e8fc4358ea74d458f5f9c
Ryan <16667079+mccaffers@users.noreply.github.com>
2026-05-16 13:02:44 +0100

    Improving code smells from sonarcloud

--- include/utilities/base64.hpp
+++ include/utilities/base64.hpp
@@ -13,8 +13,8 @@
 
 class Base64 {
 public:
-  static const std::string b64encode(const void* data, const size_t &len);
-  static const std::string b64decode(const void* data, const size_t &len);
+  static const std::string b64encode(const unsigned char* data, const size_t &len);
+  static const std::string b64decode(const unsigned char* data, const size_t &len);
   static std::string b64encode(const std::string& str);
   static std::string b64decode(const std::string& str64);
   static bool isValidBase64(const std::string& input);
